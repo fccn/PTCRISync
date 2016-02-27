3e77be289ad9c5ead4d67a20535f623586ad1d20
nmacedo <nfmmacedo@di.uminho.pt>
2016-02-27 11:29:56 +0000

    draft api

---
+++ src/pt/ptcris/PTCRISync.java
@@ -0,0 +1,18 @@
+package pt.ptcris;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.orcid.jaxb.model.record_rc2.ExternalID;
+import org.orcid.jaxb.model.record_rc2.Work;
+
+public interface PTCRISync {
+	
+	public void export(String clientID, String clientSecret, List<Work> works);
+
+	public List<Work> importWorks(String clientID, String clientSecret, Set<ExternalID> uids);
+
+	public List<ExternalID>importUIDs(String clientID, String clientSecret, Map<Object,List<ExternalID>> productions);
+    
+}
