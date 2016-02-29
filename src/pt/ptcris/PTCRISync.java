0124611c70e8da6479f0510873a9de4e8a188d4b
Alcino Cunha <alcino.cunha@gmail.com>
2016-02-29 13:37:46 +0000

    Removed import

--- src/pt/ptcris/PTCRISync.java
+++ src/pt/ptcris/PTCRISync.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.orcid.jaxb.model.record_rc2.ExternalID;
 import org.orcid.jaxb.model.record_rc2.Work;
 
 public class PTCRISync {
