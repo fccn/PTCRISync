c080807764b695628e0482915235752d6ac4e9c4
nmacedo <nfmmacedo@di.uminho.pt>
2016-05-04 14:48:47 +0100

    junit comments

--- src/tests/scenario1.java
+++ src/tests/scenario1.java
@@ -1,12 +1,7 @@
 package tests;
 
-import static org.junit.Assert.*;
-
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.SimpleFormatter;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,17 +14,15 @@ import org.orcid.jaxb.model.record_rc2.Work;
 import org.orcid.jaxb.model.record_rc2.WorkTitle;
 import org.orcid.jaxb.model.record_rc2.WorkType;
 
-import pt.ptcris.ORCIDClient;
 import pt.ptcris.ORCIDException;
 import pt.ptcris.PTCRISync;
-import pt.ptcris.handlers.ProgressHandler;
-
-import java.util.logging.Logger;
 
 public class scenario1{
 	private Profile orcid;
 	private List<Work> localWorks;
 	
+	// nmm: o perfil "0000-0003-3351-0229" deve ter exatamente um Work com um DOI e um Handle (o Work do User no cenário)
+	// nmm: o setUp deve adicionar exatamente um Work, com o access token secundário, com um DOI e um EID, sendo que o DOI é o mesmo do User (o Work do Scopus no cenário)
     @Before
 	public void setUpClass() throws Exception {
     	orcid = new Profile("0000-0003-3351-0229", "e49393b9-9494-4085-bf71-3c6bb03f3873", "PTCRIS");
@@ -38,23 +31,36 @@ public class scenario1{
     	List<Work> orcidWorks = new LinkedList<Work>();
     	orcidWorks.add(work0());
     	
+    	// nmm: aqui não podem usar o "export" para enviar os Works porque esse é um dos métodos do PTCRIS a ser testado
+    	// nmm: usem antes o ORCIDClientImpl.addWork que comunica diretamente com o ORCID
     	PTCRISync.export(orcid.orcidID, orcid.accessToken, orcidWorks, orcid.serviceSourceName, orcid.progressHandler);
     }
 	
+    // nmm: o test deve correr o import com o access token principal
+    // nmm: depois deve testar the o resultado corresponde a um Work com um DOI, um EID e um Handle (a Creation no cenário)
 	@Test
 	public void test() throws ORCIDException {
 		localWorks = new LinkedList<Work>();
+		
+		// nmm: tinhamos falado de usar tokens diferentes para o "setUp" e para o "test"
+		// nmm: caso contrário a coisa vai correr mal para quando houver IDs repetidos
 		List<Work> worksToImport = PTCRISync.importWorks(orcid.orcidID, orcid.accessToken, localWorks, orcid.progressHandler);
 
 		localWorks.addAll(worksToImport);
 		
 		orcid.progressHandler.setCurrentStatus(localWorks.toString());
 		orcid.progressHandler.done();
+		
+		// nmm: aqui agora tem que ter um "assert" que testa se o "worksToImport" é idêntico ao definido pelo cenário
 	}
 	
+	// nmm: o tearDown tem que apagar do perfil o Work com o access token secundário
+	// nmm: o perfil "0000-0003-3351-0229" fica exatamente com um Work com um DOI e um Handle 
 	@After
 	public void tearDownClass() throws Exception {
 		// Limpar o perfil;
+		
+		// nmm: têm mesmo que limpar o perfil senão os outros testes vão correr mal, por ter lá Works a mais
     }
 
 	
