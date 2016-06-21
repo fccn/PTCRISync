9a73d143d3b89632dc2dfcadae92dab7f5a57693
nmacedo <nfmmacedo@di.uminho.pt>
2016-06-21 15:44:05 +0100

    updated to spec v4.2

---
+++ src/tests/pt/ptcris/test/scenarios/Scenario16.java
@@ -0,0 +1,46 @@
+package pt.ptcris.test.scenarios;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.um.dsi.gavea.orcid.client.exception.OrcidClientException;
+import org.um.dsi.gavea.orcid.model.work.Work;
+
+import pt.ptcris.ORCIDHelper;
+
+public class Scenario16 extends Scenario {
+
+	@Override
+	List<Work> setupORCIDFixtureWorks() {
+		List<Work> works = new ArrayList<Work>();
+		works.add(ScenariosHelper.workEIDHANDLE(null, "Meta-data 3", "0", "0"));
+		return works;
+	}
+
+	@Override
+	List<Work> setupLocalWorks() {
+		List<Work> works = new ArrayList<Work>();
+		works.add(ScenariosHelper.workDOIEID(BigInteger.valueOf(1), "Meta-data 1", "0", "0"));
+		return works;
+	}
+
+	@Override
+	List<Work> expectedImportedLocalWorks() {
+		List<Work> works = new ArrayList<Work>();
+		works.add(ScenariosHelper.workHANDLE(BigInteger.valueOf(1), null, "0"));
+		works.add(ScenariosHelper.workHANDLE(BigInteger.valueOf(1), null, "1"));
+		return works;
+	}
+
+	@Override
+	ORCIDHelper clientSource() throws OrcidClientException {
+		return new ORCIDHelper(ScenarioOrcidClient.getClientWork(1));
+	}
+
+	@Override
+	ORCIDHelper clientFixture() throws OrcidClientException {
+		return new ORCIDHelper(ScenarioOrcidClient.getClientWorkFixture(1));
+	}
+
+}
