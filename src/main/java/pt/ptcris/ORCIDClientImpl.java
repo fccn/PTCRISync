272a3a7bab18e4ca2f6d7dd55bcb6a64a88772a5
nmacedo <nfmmacedo@di.uminho.pt>
2017-03-14 13:30:17 +0000

    mit license header

--- src/main/java/pt/ptcris/ORCIDClientImpl.java
+++ src/main/java/pt/ptcris/ORCIDClientImpl.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2016, 2017 PTCRIS - FCT|FCCN and others.
+ * Licensed under MIT License
+ * http://ptcris.pt
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ */
 package pt.ptcris;
 
 import java.io.Serializable;
